ff4faa423295c5f7fe97e8db7a8a92ba23d7fd34
DasuNadeema <dasunnadeema6@gmail.com>
2023-12-28 19:50:00 +0530

    Remove Extra Text

--- src/component/Movie.tsx
+++ src/component/Movie.tsx
@@ -6,26 +6,7 @@ export default function Movie() {
   return (
     <Fragment>
       <div className="movie">
-        {/* <img src={img3} alt="" className="m-img1" /> */}
         <img src={img2} alt="" className="m-img1" />
-        <div className="m-text">
-          <h4>John Wick</h4>
-          <span>2012</span>
-          <br />
-          <span>Action/Adventure</span>
-          <br />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo
-            aliquid explicabo consectetur harum pariatur soluta aliquam, autem
-            eum exercitationem dignissimos ipsum dolorum sequi tempore. Harum
-            excepturi sit eos a quasi.
-          </p>
-          <br />
-        </div>
-        <a className="a1" href="#">
-          Watch <ion-icon name="play-circle-outline"></ion-icon>
-        </a>
-        {/* <img src={img3} alt="" className="m-img2" /> */}
       </div>
       <br />
     </Fragment>
